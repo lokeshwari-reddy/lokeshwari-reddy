c70382f574d8a438dc17270fe6c6b1f6c80f8b50
lokeshwari-reddy <bheemiganilokeshwari@gmail.com>
2024-08-01 18:53:49 +0530

    Create File3.java

---
+++ Task1/Task2/File3.java
@@ -0,0 +1,50 @@
+package com.example.grocerydelivery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class GroceryItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+
+    // Constructors
+    public GroceryItem() {}
+
+    public GroceryItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
